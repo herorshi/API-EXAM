b4d07878ea97fa953dfae7934bacace3cdabc0de
herorshi <metasitstar@gmail.com>
2022-10-12 09:43:10 +0700

    remove-test-route

--- router.js
+++ router.js
@@ -1,7 +1,6 @@
 const controllerUser = require("./controller/user_list");
 function getRouter(app) {
-  app.get("/test", controllerUser.test);
-  app.get("/api", controllerUser.get_user);
+  app.get("/get_user", controllerUser.get_user);
   app.post("/add_user", controllerUser.add_user);
   app.post("/check_user", controllerUser.check_user);
   app.post("/update_user", controllerUser.update_user);
